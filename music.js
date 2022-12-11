817133166527b045d6da20cad9babb838e586c48
Megan McGee <meganpmcgee@gmail.com>
2022-12-10 22:43:53 -0800

    initial

---
+++ music.js
@@ -0,0 +1,34 @@
+const player = new Tone.Player({
+	url: "https://tonejs.github.io/audio/drum-samples/loops/ominous.mp3",
+	autostart: true,
+});
+const filter = new Tone.Filter(400, 'lowpass').toDestination();
+const feedbackDelay = new Tone.FeedbackDelay(0.125, 0.5).toDestination();
+
+// connect the player to the feedback delay and filter in parallel
+player.connect(filter);
+player.connect(feedbackDelay);
+// create two monophonic synths
+const synthA = new Tone.FMSynth().toDestination();
+const synthB = new Tone.AMSynth().toDestination();
+//play a note every quarter-note
+const loopA = new Tone.Loop(time => {
+	synthA.triggerAttackRelease("C2", "8n", time);
+}, "4n").start(0);
+//play another note every off quarter-note, by starting it "8n"
+const loopB = new Tone.Loop(time => {
+	synthB.triggerAttackRelease("C4", "8n", time);
+}, "4n").start("8n");
+// the loops start when the Transport is started
+Tone.Transport.start()
+// ramp up to 800 bpm over 10 seconds
+Tone.Transport.bpm.rampTo(800, 10);
+
+document.getElementById("play-button").addEventListener("click", function() {
+  if (Tone.context.state =='suspended') {
+    Tone.start();
+    console.log('CLICKED')
+  } else {
+    Tone.stop();
+  }
+});
